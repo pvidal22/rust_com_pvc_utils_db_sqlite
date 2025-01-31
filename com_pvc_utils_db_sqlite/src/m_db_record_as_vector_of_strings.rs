bcc6bfedeae6242e1c67102ddabb37d097d6db3d
Vidal <vidalcap@emea.roche.com>
2025-01-31 11:17:04 +0100

    v.20250131_02 1.0.5

---
+++ com_pvc_utils_db_sqlite/src/m_db_record_as_vector_of_strings.rs
@@ -0,0 +1,38 @@
+use std::ops::Deref;
+
+use crate::m_db_query_return::TToVectorOfString;
+
+#[derive(Default, Debug, Clone, PartialEq)]
+pub struct SRecordAsVectorOfStrings
+{
+    fields: Vec<String>
+}
+
+impl Deref for SRecordAsVectorOfStrings
+{
+    type Target = Vec<String>;
+
+    fn deref(&self) -> &Self::Target 
+    {
+        &self.fields
+    }
+}
+
+impl SRecordAsVectorOfStrings
+{
+    pub fn new(record: Vec<String>) -> Self
+    {
+        SRecordAsVectorOfStrings
+        {
+            fields: record,
+        }
+    }
+}
+
+impl TToVectorOfString for SRecordAsVectorOfStrings
+{
+    fn to_vector_of_strings(&self) -> Vec<String> 
+    {
+        self.fields.to_owned()
+    }
+}
